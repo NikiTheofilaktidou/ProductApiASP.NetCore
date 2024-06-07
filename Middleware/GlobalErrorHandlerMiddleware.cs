7f4f4cd2626e2042d6eea5720d63e1842fb54886
Niki Theofilaktidou <93220601+NikiTheofilaktidou@users.noreply.github.com>
2024-06-07 18:22:19 +0300

    Add files via upload

---
+++ Middleware/GlobalErrorHandlerMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+namespace ProductApi.Middleware
+{
+    public class GlobalErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (you can customize logging based on your requirements)
+
+                // Set the response status code
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                // Return a JSON response with error details
+                context.Response.ContentType = "application/json";
+                var errorResponse = new
+                {
+                    message = "An error occurred while processing your request.",
+                    error = ex.Message // Include more details if needed
+                };
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(errorResponse));
+            }
+        }
+    }
+}
