6211b5ed3420f99eb31b792e54d11b07b74f30bc
Niki Theofilaktidou <93220601+NikiTheofilaktidou@users.noreply.github.com>
2024-06-07 18:22:14 +0300

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApi.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(100, ErrorMessage = "Product name can't be longer than 100 characters.")]
+        public string? ProductName { get; set; }
+
+        [Range(0.01, 10000.00, ErrorMessage = "Price must be between 0.01 and 10000.00.")]
+        public decimal Price { get; set; }
+    }
+}
